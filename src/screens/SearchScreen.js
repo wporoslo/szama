6876a079b8b63a2bcf308b769a9cb89771e6f990
Wojciech Porosło <wporoslo@gmail.com>
2020-09-09 13:17:40 +0200

    extract business logic from view into separate helper function

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,31 +1,19 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import SearchBar from '../components/SearchBar'
-import yelp from '../api/yelp'
+import useResults from '../hooks/useResults'
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('')
-  const [results, setResults] = useState([])
-  const [errorMessage, setErrorMessage] = useState('')
-
-  const searchApi = async () => {
-    try {
-      const response = await yelp.get('/search', {
-        params: {
-          term,
-          limit: 50,
-          location: `kraków`,
-        },
-      })
-      setResults(response.data.businesses)
-    } catch (err) {
-      setErrorMessage('Something went wrong. Try again later.')
-    }
-  }
+  const [searchApi, results, errorMessage] = useResults()
 
   return (
     <View style={styles.search}>
-      <SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchApi} />
+      <SearchBar
+        term={term}
+        onTermChange={setTerm}
+        onTermSubmit={() => searchApi(term)}
+      />
       {results ? <Text>We have found {results.length} results.</Text> : null}
       {errorMessage ? <Text>{errorMessage}</Text> : null}
     </View>
